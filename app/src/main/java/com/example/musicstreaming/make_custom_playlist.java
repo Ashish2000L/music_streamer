c4e5cef117fa2c66cc8bb4baf8bd551de5dbbd36
Ashish kumar <54790511+Ashish2000L@users.noreply.github.com>
2020-11-13 22:11:56 +0530

    added new file

---
+++ app/src/main/java/com/example/musicstreaming/make_custom_playlist.java
@@ -0,0 +1,14 @@
+package com.example.musicstreaming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class make_custom_playlist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_custom_playlist);
+    }
+}
