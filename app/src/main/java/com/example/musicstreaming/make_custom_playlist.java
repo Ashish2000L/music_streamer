8f0bc2dd219970446eb016092c1b20bbc9a7a645
Ashish kumar <54790511+Ashish2000L@users.noreply.github.com>
2020-12-07 16:42:48 +0530

    Update make_custom_playlist.java

--- app/src/main/java/com/example/musicstreaming/make_custom_playlist.java
+++ app/src/main/java/com/example/musicstreaming/make_custom_playlist.java
@@ -2,13 +2,26 @@ package com.example.musicstreaming;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import static com.example.musicstreaming.MainActivity.MAIN_ACTIVITY;
 
 public class make_custom_playlist extends AppCompatActivity {
+
+    ListView custom_listview;
+    listofplaylist playlist;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_custom_playlist);
+
+        Thread.setDefaultUncaughtExceptionHandler(new Exceptionhandler(this));
+
+
+
     }
 }
