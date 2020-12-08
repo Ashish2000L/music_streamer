3282b2aaf82226dde1edee717770822ce016df15
Ashish kumar <54790511+Ashish2000L@users.noreply.github.com>
2020-12-08 17:46:29 +0530

    added fragment manager

--- app/src/main/java/com/example/musicstreaming/make_custom_playlist.java
+++ app/src/main/java/com/example/musicstreaming/make_custom_playlist.java
@@ -1,27 +1,36 @@
 package com.example.musicstreaming;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import static com.example.musicstreaming.MainActivity.MAIN_ACTIVITY;
 
 public class make_custom_playlist extends AppCompatActivity {
 
-    ListView custom_listview;
-    listofplaylist playlist;
-
-
+    public static Activity MAKE_NEW_PLAYLIST;
+    Fragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_custom_playlist);
 
-        Thread.setDefaultUncaughtExceptionHandler(new Exceptionhandler(this));
-
-
-
+        MAKE_NEW_PLAYLIST=this;
+        Toolbar toolbar = findViewById(R.id.new_playlist_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportFragmentManager().beginTransaction().replace(R.id.custom_frame_layout,new select_songs_for_playlist()).commit();
     }
+
+
 }
