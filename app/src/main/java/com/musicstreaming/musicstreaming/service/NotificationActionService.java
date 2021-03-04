6e0ca2868ef46e127cf7d026e3444f4a65a2cdee
Ashish kumar <54790511+Ashish2000L@users.noreply.github.com>
2021-03-04 18:19:42 +0530

    added night_mode file for the night mode

--- app/src/main/java/com/musicstreaming/musicstreaming/service/NotificationActionService.java
+++ app/src/main/java/com/musicstreaming/musicstreaming/service/NotificationActionService.java
@@ -3,6 +3,7 @@ package com.musicstreaming.musicstreaming.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.view.KeyEvent;
 
 public class NotificationActionService extends BroadcastReceiver {
     /**
--- app/src/main/java/com/musicstreaming/musicstreaming/setting_advance.java
+++ app/src/main/java/com/musicstreaming/musicstreaming/setting_advance.java
@@ -4,14 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.widget.CompoundButton;
 
 import java.io.File;
 
+import static com.musicstreaming.musicstreaming.MainActivity.MAIN_ACTIVITY;
+import static com.musicstreaming.musicstreaming.login.PASSWORD;
 import static com.musicstreaming.musicstreaming.login.SHARED_PREF;
+import static com.musicstreaming.musicstreaming.service.onclearfrompercentservice.TERMINATION_STATUS;
+import static com.musicstreaming.musicstreaming.service.onclearfrompercentservice.handler;
+import static com.musicstreaming.musicstreaming.service.onclearfrompercentservice.isplaying;
+import static com.musicstreaming.musicstreaming.service.onclearfrompercentservice.isprepared;
+import static com.musicstreaming.musicstreaming.service.onclearfrompercentservice.ispreparing;
+import static com.musicstreaming.musicstreaming.service.onclearfrompercentservice.terminate_all_process;
 import static com.musicstreaming.musicstreaming.splash.DIR_NAME;
 import static com.musicstreaming.musicstreaming.splash.NIGHT_MODE;
 import static com.musicstreaming.musicstreaming.splash.USERNAME;
@@ -25,9 +35,12 @@ public class setting_advance extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_advance);
 
+        Thread.setDefaultUncaughtExceptionHandler(new Exceptionhandler(this));
+
         switchCompat=findViewById(R.id.night_mode);
 
         sharedPreferences=getSharedPreferences(SHARED_PREF,MODE_PRIVATE);
+
         night_mode_setting();
 
     }
@@ -39,25 +52,47 @@ public class setting_advance extends AppCompatActivity {
 
         switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            public void onCheckedChanged(CompoundButton buttonView, final boolean isChecked) {
 
-                if(isChecked){
-                    switchCompat.setChecked(true);
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                    editor.putBoolean(NIGHT_MODE,true);
-                    editor.apply();
+                terminate_all_process.run();
+                Runnable check_termination_status = new Runnable() {
+                    @Override
+                    public void run() {
+                        if(TERMINATION_STATUS) {
 
-                    new make_file_in_directory(setting_advance.this,getApplicationContext(),sharedPreferences.getString(USERNAME,"")).write_night_mode(new File(Environment.getExternalStorageDirectory()+"/"+DIR_NAME,NIGHT_MODE),true);
+                            if(isChecked){
+                                switchCompat.setChecked(true);
+                                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                                editor.putBoolean(NIGHT_MODE,true);
+                                editor.apply();
 
-                }else{
-                    switchCompat.setChecked(false);
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                    editor.putBoolean(NIGHT_MODE,false);
-                    editor.apply();
 
-                    new make_file_in_directory(setting_advance.this,getApplicationContext(),sharedPreferences.getString(USERNAME,"")).write_night_mode(new File(Environment.getExternalStorageDirectory()+"/"+DIR_NAME,NIGHT_MODE),false);
+                                new make_file_in_directory(setting_advance.this,getApplicationContext(),sharedPreferences.getString(USERNAME,"")).write_night_mode(new File(Environment.getExternalStorageDirectory()+"/"+DIR_NAME,NIGHT_MODE),true);
 
-                }
+                                Log.d("testing_size", "run: homefragment: "+homefragment.listofplaylistArrayList.size()+" songsfromplaylist : "+songsfromplaylist.listofsongsArrayLisr.size());
+
+                            }else{
+
+                                switchCompat.setChecked(false);
+                                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                                editor.putBoolean(NIGHT_MODE,false);
+                                editor.apply();
+
+                                new make_file_in_directory(setting_advance.this,getApplicationContext(),sharedPreferences.getString(USERNAME,"")).write_night_mode(new File(Environment.getExternalStorageDirectory()+"/"+DIR_NAME,NIGHT_MODE),false);
+
+                                Log.d("testing_size", "run: homefragment: "+homefragment.listofplaylistArrayList.size()+" songsfromplaylist : "+songsfromplaylist.listofsongsArrayLisr.size());
+                            }
+
+                            startActivity(new Intent(setting_advance.this,MainActivity.class));
+                            finish();
+
+                        }else{
+                            handler.postDelayed(this,1000);
+                        }
+                    }
+                };
+
+                check_termination_status.run();
 
             }
         });
