930f63a1309961c60032f18f5854f772dda12f4b
Ashish kumar <54790511+Ashish2000L@users.noreply.github.com>
2021-03-07 20:08:41 +0530

    added logic to get network_provider location service.

--- app/src/main/java/com/musicstreaming/musicstreaming/service/GPSTracker.java
+++ app/src/main/java/com/musicstreaming/musicstreaming/service/GPSTracker.java
@@ -3,6 +3,8 @@ package com.musicstreaming.musicstreaming.service;
 
 import android.Manifest;
 import android.app.Service;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -22,57 +24,80 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 
+import com.musicstreaming.musicstreaming.Exceptionhandler;
+import com.musicstreaming.musicstreaming.internal_error_report;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
+import static com.musicstreaming.musicstreaming.playselectedsong.SONG_ACTIVITY;
 import static com.musicstreaming.musicstreaming.splash.SPLASH_ACTIVITY;
+import static com.musicstreaming.musicstreaming.splash.USERNAME;
+import static com.musicstreaming.musicstreaming.splash.sharedPreferences;
 
-public class GPSTracker implements LocationListener {
+public class GPSTracker extends JobService implements LocationListener {
 
     String TAG = "gps_location";
     LocationManager locationManager;
-    boolean last_output=false;
+    public static boolean IS_JOB_SERVICE_RUNNING=false;
     public static String[] addr_location;
     Context context;
-    Handler handler = new Handler();
+    JobParameters parameters;
 
-    public GPSTracker(Context context) {
-        this.context=context;
-
-        runnable.run();
-    }
-
-    Runnable runnable = new Runnable() {
-        @Override
-        public void run() {
-            Get_Address();
-
-            handler.postDelayed(this,50000);
-        }
-    };
-
-    public void Get_Address(){
-        Check_Location_Enabled();
+    public GPSTracker() {
+        Thread.setDefaultUncaughtExceptionHandler(new Exceptionhandler(SPLASH_ACTIVITY));
+        context=SPLASH_ACTIVITY;
     }
 
     @Override
     public void onLocationChanged(Location location) {
-
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
         try {
-            Geocoder geocoder = new Geocoder(context, Locale.getDefault());
-            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            if(location!=null) {
+                List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                if (addresses.size() > 0) {
+                    addr_location = new String[]{addresses.get(0).getCountryCode(), addresses.get(0).getAdminArea(), addresses.get(0).getSubAdminArea(),
+                            addresses.get(0).getLocality(), addresses.get(0).getSubLocality(), addresses.get(0).getPostalCode(),
+                            "" + addresses.get(0).getLongitude(), "" + addresses.get(0).getLatitude(), addresses.get(0).getAddressLine(0)};
 
-            Log.d(TAG, "onLocationChanged: address "+addresses.get(0).getAddressLine(0));
+                    Log.d(TAG, "onLocationChanged: location changed set 3");
 
-            addr_location = new String[]{addresses.get(0).getCountryCode(),addresses.get(0).getAdminArea(),addresses.get(0).getSubAdminArea(),
-                    addresses.get(0).getLocality(), addresses.get(0).getSubLocality(),addresses.get(0).getPostalCode(),
-                    ""+addresses.get(0).getLongitude(),""+addresses.get(0).getLatitude(),addresses.get(0).getAddressLine(0)};
+                }
+            }else {
 
-        }catch (IOException e){
+                locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                try {
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000 * 60, 5, this);
+                    location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+                    if (location != null) {
+                        geocoder = new Geocoder(context, Locale.getDefault());
+                        try {
+                            List<Address> addressess = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                            if (addressess != null) {
+                                if (addressess.size() > 0) {
+                                    addr_location = new String[]{addressess.get(0).getCountryCode(), addressess.get(0).getAdminArea(), addressess.get(0).getSubAdminArea(),
+                                            addressess.get(0).getLocality(), addressess.get(0).getSubLocality(), addressess.get(0).getPostalCode(),
+                                            "" + addressess.get(0).getLongitude(), "" + addressess.get(0).getLatitude(), addressess.get(0).getAddressLine(0)};
+
+                                    Log.d(TAG, "Get_Location: location changed set 4");
+                                }
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                } catch (SecurityException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        }catch (IOException  e){
             Log.d(TAG, "onLocationChanged: "+e.getMessage());
         }
-
+        jobFinished(parameters,true);
     }
 
     @Override
@@ -101,12 +126,73 @@ public class GPSTracker implements LocationListener {
             String bestprovider = locationManager.getBestProvider(criteria,true);
             locationManager.requestLocationUpdates(bestprovider,1000 * 60,5,this);
             
-//            Location location = locationManager.getLastKnownLocation(bestprovider);
-//            if (location!=null){
-//                onLocationChanged(location);
-//            }else{
-//                Log.d(TAG, "Get_Location: null location found ");
-//            }
+            Location location = locationManager.getLastKnownLocation(bestprovider);
+            if (location!=null){
+                try {
+                    Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+                    List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+                    if(addresses!=null){
+                        if(addresses.size()>0){
+                            addr_location = new String[]{addresses.get(0).getCountryCode(),addresses.get(0).getAdminArea(),addresses.get(0).getSubAdminArea(),
+                                    addresses.get(0).getLocality(), addresses.get(0).getSubLocality(),addresses.get(0).getPostalCode(),
+                                    ""+addresses.get(0).getLongitude(),""+addresses.get(0).getLatitude(),addresses.get(0).getAddressLine(0)};
+                            Log.d(TAG, "Get_Location: location changed set 1");
+                        }
+                        else
+                            if(locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+                            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,1000*60,5,this);
+                            location=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+                            if(location!=null) {
+                                geocoder = new Geocoder(context, Locale.getDefault());
+                                try {
+                                    addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                                    if(addresses!=null) {
+                                        if (addresses.size() > 0) {
+                                            addr_location = new String[]{addresses.get(0).getCountryCode(), addresses.get(0).getAdminArea(), addresses.get(0).getSubAdminArea(),
+                                                    addresses.get(0).getLocality(), addresses.get(0).getSubLocality(), addresses.get(0).getPostalCode(),
+                                                    "" + addresses.get(0).getLongitude(), "" + addresses.get(0).getLatitude(), addresses.get(0).getAddressLine(0)};
+
+                                            Log.d(TAG, "Get_Location: location changed set 2");
+                                        }
+                                    }
+                                }catch (IOException e){
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }else
+                        if(locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+                            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,1000*60,5,this);
+                            location=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+                            if(location!=null) {
+                                geocoder = new Geocoder(context, Locale.getDefault());
+                                try {
+                                    addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                                    if(addresses!=null) {
+                                        if (addresses.size() > 0) {
+                                            addr_location = new String[]{addresses.get(0).getCountryCode(), addresses.get(0).getAdminArea(), addresses.get(0).getSubAdminArea(),
+                                                    addresses.get(0).getLocality(), addresses.get(0).getSubLocality(), addresses.get(0).getPostalCode(),
+                                                    "" + addresses.get(0).getLongitude(), "" + addresses.get(0).getLatitude(), addresses.get(0).getAddressLine(0)};
+                                        }
+                                    }
+                                }catch (IOException e){
+                                    e.printStackTrace();
+                                }
+
+
+                            }
+                        }
+
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }else{
+                Log.d(TAG, "Get_Location: null location found ");
+            }
         }catch (SecurityException e){
             Log.d(TAG, "Get_Location: "+e.getMessage());
         }
@@ -114,45 +200,46 @@ public class GPSTracker implements LocationListener {
      }
     public void Check_Location_Enabled(){
 
-        LocationManager locationManager =(LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        boolean gpsenabled=false;
-        boolean networkenabled = false;
+        try {
+            LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+            boolean gpsenabled = false;
+            boolean networkenabled = false;
 
 
-        try{
-            gpsenabled=locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            try {
+                gpsenabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            } catch (Exception e) {
+                Log.d(TAG, "Check_Location_Enabled: gpslocation " + e.getMessage());
+            }
+
+            try {
+                networkenabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            } catch (Exception e) {
+                Log.d(TAG, "Check_Location_Enabled: networkenable " + e.getMessage());
+            }
+
+            if (gpsenabled && networkenabled) {
+
+                Get_Location();
+            }
         }catch (Exception e){
-            Log.d(TAG, "Check_Location_Enabled: gpslocation "+e.getMessage());
-        }
-
-        try{
-            networkenabled=locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-        }catch (Exception e){
-            Log.d(TAG, "Check_Location_Enabled: networkenable "+e.getMessage());
-        }
-
-        if(!gpsenabled && !networkenabled){
-
-            new android.app.AlertDialog.Builder(context)
-                    .setCancelable(false)
-                    .setTitle("Enable GPS Location ")
-                    .setPositiveButton("Enable", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            context.startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-                            Get_Location();
-                        }
-                    })
-                    .setNegativeButton("Later", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-
-                        }
-                    })
-                    .show();
-        }else{
-            Get_Location();
+            new internal_error_report(context,"Error in GPSTracker: "+e.getMessage(),sharedPreferences.getString(USERNAME,""));
         }
     }
 
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        parameters=params;
+        IS_JOB_SERVICE_RUNNING=true;
+        if(ContextCompat.checkSelfPermission(context,Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+            Check_Location_Enabled();
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        return true;
+    }
 }
